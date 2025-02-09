65bef6b07a24ab9c02686668095b567f58d4b7e2
Ben Johnson <johben17796@gmail.com>
2025-02-09 17:05:12 -0600

    seeded

--- Develop/server/src/seeds/cleanDb.ts
+++ Develop/server/src/seeds/cleanDb.ts
@@ -7,8 +7,8 @@ export default async (modelName: "Question", collectionName: string) => {
       name: collectionName
     }).toArray()
 
-    if (modelExists.length) {
-      await db.dropCollection(collectionName);
+    if (modelExists?.length) {
+      await db?.dropCollection(collectionName);
     }
   } catch (err) {
     throw err;
