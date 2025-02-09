83349ac17fecd30736dbd1b7199f8cdbe6f63c96
Ben Johnson <johben17796@gmail.com>
2025-02-09 16:58:05 -0600

    git ignore

--- Develop/server/src/seeds/cleanDb.ts
+++ Develop/server/src/seeds/cleanDb.ts
@@ -3,7 +3,7 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
+    const modelExists = await models[modelName]?.db?.db?.listCollections({
       name: collectionName
     }).toArray()
 
